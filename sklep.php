c530646963d16de08b4dc84a4585951dc3e54bae
Fiedor_ <stachfie@wp.pl>
2023-06-03 12:32:32 +0200

    Napisany html

---
+++ sklep.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Warzywniak</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<header>
+    <div id="hl">
+        <h1>Internetowy sklep z eko-warzywami</h1>
+    </div>
+    <div id="hr">
+        <ol>
+            <li>warzywa</li>
+            <li>owoce</li>
+            <li><a href="https://terapiasokami.pl/" target="blank">soki</a></li>
+        </ol>
+    </div>
+</header>
+<main>
+    <?php
+
+    ?>
+</main>
+<footer>
+    <form action="sklep.php" method="post">
+        <label for="nazwa">Nazwa</label>
+        <input type="text" name="nazwa">
+        <label for="nazwa">Cena</label>
+        <input type="number" name="cena">
+        <input type="submit" value="Dodaj produkt">
+    </form>
+    Stronę wykonał: Stanisław Fiedoruk 4J
+</footer>
+<?php
+?>
+
+<body>
+
+</body>
+
+</html>
